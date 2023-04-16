aef230422f53f25c9e500ed3b727ade370d5f63c
Ricardo Castanho <48383095+ricardocastanho@users.noreply.github.com>
2023-04-16 11:06:54 -0300

    feat: add debounce utils

---
+++ utils/debounce.ts
@@ -0,0 +1,18 @@
+export function debounce<T extends (...args: any[]) => void>(
+  fn: T,
+  delay: number
+): (...args: Parameters<T>) => void {
+  let timeoutId: ReturnType<typeof setTimeout> | null = null;
+
+  const debouncedFn = function (this: unknown, ...args: Parameters<T>) {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+      fn.apply(this, args);
+    }, delay);
+  };
+
+  return debouncedFn;
+}
