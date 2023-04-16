4270f914ba06eb89b4c627591320ba8e375675cc
Ricardo Castanho <48383095+ricardocastanho@users.noreply.github.com>
2023-04-16 11:07:57 -0300

    feat: add pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  imports: {
+    dirs: ["stores/**", "utils/contracts/**"],
+  },
+  modules: ["@pinia/nuxt"],
+  pinia: {
+    autoImports: [
+      "defineStore",
+      "storeToRefs",
+      ["defineStore", "definePiniaStore"],
+    ],
+  },
+});
